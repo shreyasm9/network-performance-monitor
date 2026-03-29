ea57d6e484047922a195732177780353b8ff2eff
shreyasm9 <mshreyas683@gmail.com>
2026-03-29 16:13:46 +0530

    Added alert system and improved dashboard UI

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 import csv
 
 app = Flask(__name__)
+app.secret_key = "secret123"
+
+USERNAME = "admin"
+PASSWORD = "admin"
 
 def read_data():
     data = []
@@ -11,14 +14,37 @@ def read_data():
             reader = csv.reader(file)
             for row in reader:
                 data.append(row)
-    except FileNotFoundError:
+    except:
         pass
     return data
 
-@app.route("/")
-def index():
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = request.form["username"]
+        pwd = request.form["password"]
+
+        if user == USERNAME and pwd == PASSWORD:
+            session["logged_in"] = True
+            return redirect("/dashboard")
+        else:
+            return "Invalid credentials"
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if not session.get("logged_in"):
+        return redirect("/")
+
     data = read_data()
+    print("DEBUG DATA:", data)
     return render_template("index.html", data=data)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
